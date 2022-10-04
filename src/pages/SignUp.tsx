6034d2549a165ec6ea6c9e20e9881f179d8eacd8
Carlos Joco Gumanay <jocogum@DESKTOP-15283M7.localdomain>
2022-10-04 20:48:36 +0800

    add log in feature

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -47,9 +47,6 @@ function SignUp() {
     });
   }
 
-  // debug
-  console.log(user);
-
   return (
     <div>
       <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
