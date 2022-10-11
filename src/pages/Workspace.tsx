b3e7958b843114be75e9086d0a469c4d33be5e34
Carlos Joco Gumanay <jocogum@DESKTOP-15283M7.localdomain>
2022-10-11 19:46:36 +0800

    add workspaces interactivity

--- src/pages/Workspace.tsx
+++ src/pages/Workspace.tsx
@@ -11,36 +11,48 @@ function Workspace() {
   const [data, setData] = useState([]);
   const [error, setError] = useState([])
 
-  useEffect( ()=> {
+  useEffect(() => {
     retrieveJwt();
-    axios.get(BASE_URL+ '/api/v1/workspaces',{
+    axios.get(BASE_URL + '/api/v1/workspaces', {
       headers: {
         'Authorization': retrieveJwt(),
       }
-    }).then( (response)=> {
+    }).then((response) => {
       setData(response.data.data);
-      // console.log(response)
-    }).catch( error => {
+    }).catch(error => {
       setError(error);
     });
   }, []);
 
-  console.log(data[0]);
+  // console.log(data);
 
-  // const cards = data.map( ({data, idx}) => (
-  //   <WorkspaceCard name={data.attributes} description={data.attributes}/>
-  // ))
+  const cards = data.map((data) => {
+    return <WorkspaceCard key={data["id"]} name={data["attributes"]["name"]} description={data["attributes"]["description"]} />
+  });
 
-  
-  // data.map( (d) => {
-  //   console.log(d)
-  //   // return <WorkspaceCard name={d.attributes.name} description={d.attributes.description}/>
-  // });
-  
   return (
     <div>
       <Header />
-      {/* {cards} */}
+
+      <section className="bg-white shadow flex justify-between content-center">
+        <div className="py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold tracking-tight text-indigo-600">Workspaces</h1>
+        </div>
+        <div className='py-6 px-4 sm:px-6 lg:px-8'>
+          <button
+            type="submit"
+            className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            + New Workspace
+          </button>
+        </div>
+      </section>
+      <main>
+        {data && cards}
+      </main>
+
+
+
     </div>
   )
 }
